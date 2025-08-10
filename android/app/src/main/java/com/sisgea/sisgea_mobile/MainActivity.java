e54c4348985ad440cf4ccdb1e96f28e41ad2ec39
LucaJust115 <802.569@alunos.unigran.br>
2025-08-10 19:08:15 -0400

    Início do Projeto

---
+++ android/app/src/main/java/com/sisgea/sisgea_mobile/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sisgea.sisgea_mobile;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/sisgea/sisgea_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sisgea.sisgea_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
